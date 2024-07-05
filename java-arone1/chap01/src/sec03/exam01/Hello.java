edf0eea49712031e758dc89eabc97430cb5020f0
tjddnsrla <@naver.com>
2024-07-05 11:25:59 +0900

    처음 업로드1

---
+++ java-arone1/chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,19 @@
+package sec03.exam01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println(" //Hello, Java"); 
+		
+        int x;                            // 변수 x선언
+        x = 1;                            // 변수 x에 1을 저장
+        int y = 2;                        // 변수 y를 선언하고 2를 저장
+        int result = x + y; // 1 + 2 = 3  // 변수 result를 선언하고 변수 x와 y를 더한 값을 저장
+        System.out.println(result);       // println 메소드 호출
+
+		/*
+        int x = 1; int y = 2;
+        int result =
+        x + y;
+        */
+	}
+}
