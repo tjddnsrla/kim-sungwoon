86e94eff2d78e173e9eaf5f66db9f0bf367f8551
tjddnsrla <@naver.com>
2024-07-25 09:47:55 +0900

    처음 업로드1

---
+++ full-starck1/starbucks-homepage1/src/main/webapp/js/notice.js
@@ -0,0 +1,10 @@
+function searchNotice() {
+	const searchTextBoxEl = document.querySelector('#sch_bar');
+
+	window.location.href = '/starbucks-homepage1/notice_list.jsp?search=' + searchTextBoxEl.value;
+}
+
+
+
+
+
