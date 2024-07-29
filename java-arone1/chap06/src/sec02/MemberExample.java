b5926152967038bcc6541762e97c5eda9549f30b
tjddnsrla <@naver.com>
2024-07-29 16:48:30 +0900

    처음 업로드1

---
+++ java-arone1/chap06/src/sec02/MemberExample.java
@@ -0,0 +1,12 @@
+package sec02;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member member = new Member();
+		member.name = "최하얀";
+		member.age = 23;
+	}
+
+}
